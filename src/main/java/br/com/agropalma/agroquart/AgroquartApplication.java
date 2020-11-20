101a10fdf5ca092270718333b1839548e11138b8
Luiz Filipy <luizfilipy.aa@outlook.com>
2020-11-20 19:55:40 -0300

    Primeiro commit - Ajustes para banco de dados, servidor e templates

---
+++ src/main/java/br/com/agropalma/agroquart/AgroquartApplication.java
@@ -0,0 +1,13 @@
+package br.com.agropalma.agroquart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgroquartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgroquartApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/agropalma/agroquart/AgroquartApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.agropalma.agroquart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgroquartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
