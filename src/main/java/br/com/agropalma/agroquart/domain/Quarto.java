da152334f525aa27dba7bfe1431bf0548b7ebb61
Luiz Filipy <luizfilipy.aa@outlook.com>
2020-11-24 19:27:51 -0300

    alterando a geração de ids (autoincremento)

--- src/main/java/br/com/agropalma/agroquart/domain/Quarto.java
+++ src/main/java/br/com/agropalma/agroquart/domain/Quarto.java
@@ -4,6 +4,8 @@ import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
@@ -25,13 +27,14 @@ import java.util.List;
 public class Quarto {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Long id;
 
     @Column(name = "capacidade")
     private int capacidade;
 
-    @ManyToOne(fetch = FetchType.LAZY, cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
+    @ManyToOne(fetch = FetchType.EAGER, cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
     @JoinColumn(name = "casa_id")
     private Casa casa;
 
